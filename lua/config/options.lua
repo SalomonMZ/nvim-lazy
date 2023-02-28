175fae7a783d927a5819604b4f872199d8918589
Salomon Moreno <msalomon@vmware.com>
2023-02-28 14:38:08 -0500

    Added plugin for autofolding along with necessary lsp config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+-- folding config
+vim.opt.foldcolumn = "0"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = -1
+vim.opt.foldenable = true
